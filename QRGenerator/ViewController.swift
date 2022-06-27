8ad63e8de5d61be0801e8951c64d6a3457d6b41b
Marco Alonso <marco@garagecoders.net>
2022-06-27 14:49:33 -0500

    Simple Image QR Static Text

---
+++ QRGenerator/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  QRGenerator
+//
+//  Created by marco rodriguez on 27/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imagenQR: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagenQR.image = generateQRCode(from: "Hacking with Swift is the best iOS coding tutorial I've ever read!")
+    }
+
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
+
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+
+        return nil
+    }
+
+    
+
+}
+
